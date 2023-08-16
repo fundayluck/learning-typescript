cba8b155fd824cd87131efa95ca41a5ebd31a528
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-08-16 12:50:28 +0700

    setup typescript project

---
+++ tests/hello.test.ts
@@ -0,0 +1,6 @@
+describe('Hello', () => { 
+    it("should say hello", function() {
+        const name = "Hello Afan Dayu Laksono!";
+        expect(name).toBe("Hello Afan Dayu Laksono!")
+    })
+ })
