3bbc31b6e57c2026dcea98f860d8d6a6b84e7ead
Afan Dayu Laksono <48554914+fundayluck@users.noreply.github.com>
2023-08-16 13:32:48 +0700

    Primitive data types

---
+++ tests/typedata.test.ts
@@ -0,0 +1,10 @@
+describe('Data Type', () => {
+    it('should must declare', () => {
+        const name: string = "Afan Dayu Laksono"
+        const balance: number = 100000
+        const isVip: boolean = true
+        console.info(name)
+        console.info(balance)
+        console.info(isVip)
+    })
+})
